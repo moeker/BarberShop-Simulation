1b7c4e34ddb4a170cceed4ce051d8db00e70cb73
moeker <m.keraidy@gmail.com>
2017-06-17 02:17:28 +0200

    Add files via upload

---
+++ BarberShop2/BarberShop2/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace BarberShop2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.standingArea = new System.Windows.Forms.ListBox();
+            this.sofaList = new System.Windows.Forms.ListBox();
+            this.eventLog = new System.Windows.Forms.ListBox();
+            this.cashierList = new System.Windows.Forms.ListBox();
+            this.outside = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button2.Location = new System.Drawing.Point(13, 166);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 29);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Maroon;
+            this.button1.Location = new System.Drawing.Point(13, 201);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 29);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label6.Location = new System.Drawing.Point(456, 24);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 17);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Cashier";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label5.Location = new System.Drawing.Point(582, 24);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 17);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Left The Shop";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label4.Location = new System.Drawing.Point(306, 173);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 17);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Sofa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label2.Location = new System.Drawing.Point(138, 24);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Event Log";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Entered The Shop";
+            // 
+            // standingArea
+            // 
+            this.standingArea.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.standingArea.ForeColor = System.Drawing.Color.Black;
+            this.standingArea.FormattingEnabled = true;
+            this.standingArea.ItemHeight = 15;
+            this.standingArea.Location = new System.Drawing.Point(16, 40);
+            this.standingArea.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.standingArea.Name = "standingArea";
+            this.standingArea.Size = new System.Drawing.Size(120, 379);
+            this.standingArea.TabIndex = 27;
+            this.standingArea.SelectedIndexChanged += new System.EventHandler(this.lst_OD_SelectedIndexChanged);
+            // 
+            // sofaList
+            // 
+            this.sofaList.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sofaList.ForeColor = System.Drawing.Color.Black;
+            this.sofaList.FormattingEnabled = true;
+            this.sofaList.ItemHeight = 15;
+            this.sofaList.Location = new System.Drawing.Point(306, 192);
+            this.sofaList.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.sofaList.Name = "sofaList";
+            this.sofaList.Size = new System.Drawing.Size(146, 229);
+            this.sofaList.TabIndex = 29;
+            // 
+            // eventLog
+            // 
+            this.eventLog.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eventLog.ForeColor = System.Drawing.Color.Black;
+            this.eventLog.FormattingEnabled = true;
+            this.eventLog.ItemHeight = 15;
+            this.eventLog.Location = new System.Drawing.Point(142, 40);
+            this.eventLog.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.eventLog.Name = "eventLog";
+            this.eventLog.Size = new System.Drawing.Size(311, 124);
+            this.eventLog.TabIndex = 30;
+            // 
+            // cashierList
+            // 
+            this.cashierList.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cashierList.ForeColor = System.Drawing.Color.Black;
+            this.cashierList.FormattingEnabled = true;
+            this.cashierList.ItemHeight = 15;
+            this.cashierList.Location = new System.Drawing.Point(459, 40);
+            this.cashierList.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.cashierList.Name = "cashierList";
+            this.cashierList.Size = new System.Drawing.Size(120, 379);
+            this.cashierList.TabIndex = 31;
+            // 
+            // outside
+            // 
+            this.outside.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outside.ForeColor = System.Drawing.Color.Black;
+            this.outside.FormattingEnabled = true;
+            this.outside.ItemHeight = 15;
+            this.outside.Location = new System.Drawing.Point(585, 40);
+            this.outside.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.outside.Name = "outside";
+            this.outside.Size = new System.Drawing.Size(120, 379);
+            this.outside.TabIndex = 32;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.groupBox1.Location = new System.Drawing.Point(142, 180);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.groupBox1.Size = new System.Drawing.Size(158, 241);
+            this.groupBox1.TabIndex = 33;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter Numbers:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(10, 122);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 15);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "No of Sofa Places:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(10, 72);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 15);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Max Capacity:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(10, 26);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "No of Customers:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 138);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(148, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 88);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 42);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 435);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.outside);
+            this.Controls.Add(this.cashierList);
+            this.Controls.Add(this.eventLog);
+            this.Controls.Add(this.sofaList);
+            this.Controls.Add(this.standingArea);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox standingArea;
+        private System.Windows.Forms.ListBox sofaList;
+        private System.Windows.Forms.ListBox eventLog;
+        private System.Windows.Forms.ListBox cashierList;
+        private System.Windows.Forms.ListBox outside;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ BarberShop2/BarberShop2/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace BarberShop2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        ThreadStart TH;
+        Thread []customers;
+        Thread []barbers;
+        Thread []cashiers;
+        Semaphore max_capacity;
+        Semaphore sofa;
+        Semaphore barberChair;
+        Semaphore coordinator;
+        Semaphore custReady;
+        Semaphore leaveChair;
+        Semaphore payment;
+        Semaphore recipt;
+        Semaphore[] finished;
+        Semaphore mutex1;
+        Semaphore mutex2;
+        Queue Q1;
+        Queue Q2;
+        int count;
+        int nCustomers;
+        int nCap;
+        int nSofa;
+        int nBarbers = 3;
+        int nCashier = 1;
+       
+        public void customer()
+        {
+            int cust_nr;
+            cust_nr = count++;
+            max_capacity.Wait();
+            standingArea.Items.Add("Customer " + cust_nr.ToString());
+            mutex1.Wait();
+            Thread.Sleep(500);
+            standingArea.Items.Remove("Customer " + cust_nr.ToString());    
+            Thread.Sleep(500);
+            mutex1.Signal();
+            sofa.Wait();
+            sofaList.Items.Add("Customer " + cust_nr.ToString());
+            Thread.Sleep(500);
+            barberChair.Wait();
+            Thread.Sleep(500);
+            sofaList.Items.Remove("Customer " + cust_nr.ToString());
+            sofa.Signal();
+            eventLog.Items.Add("Customer " + cust_nr.ToString() + " has started hair cutting");
+            Thread.Sleep(500);
+            mutex2.Wait();
+            //Thread.Sleep(500);
+            Q1.Enqueue((int)cust_nr);
+            mutex2.Signal();
+            Thread.Sleep(500);
+            custReady.Signal();
+            Thread.Sleep(500);
+            finished[cust_nr - 1].Wait();
+            Thread.Sleep(500);
+            eventLog.Items.Add("Customer " + cust_nr.ToString() + "has finished hair cutting");
+            Thread.Sleep(500);
+            leaveChair.Signal();
+            Thread.Sleep(500);
+            cashierList.Items.Add("Customer " + cust_nr.ToString());
+            payment.Signal();
+            recipt.Wait();
+            cashierList.Items.Remove("Customer " + cust_nr.ToString());
+            outside.Items.Add("Customer " + cust_nr.ToString());
+            max_capacity.Signal();
+
+        }
+        public void barber()
+        {
+            
+            while (true)
+            {
+                int b_cust = 0;
+                Thread.Sleep(500);
+                custReady.Wait();
+                Thread.Sleep(500);
+                mutex2.Wait();
+                Thread.Sleep(500);
+                b_cust= (int)Q1.Dequeue();
+                Q2.Enqueue((int) b_cust);
+                mutex2.Signal();
+                Thread.Sleep(500);
+                coordinator.Wait();
+                //lst_BA.Items.Add("Barber is cutting hair for customer"+b_cust.ToString());
+                Thread.Sleep(500);  
+                coordinator.Signal();
+                Thread.Sleep(500);
+                finished[b_cust-1].Signal();
+                Thread.Sleep(500);
+                leaveChair.Wait();
+                Thread.Sleep(500);
+                barberChair.Signal();
+            }
+        }
+        public void cashier()
+        {
+            int count = 0;
+
+            while (true)
+            {
+                
+                Thread.Sleep(500);
+                payment.Wait();
+                Thread.Sleep(500);
+                coordinator.Wait();
+                count = (int)Q2.Dequeue();
+                Thread.Sleep(500);
+                eventLog.Items.Add("Payment of cust ("+count.ToString()+ ")is accepted");
+                Thread.Sleep(500);
+                coordinator.Signal();
+                Thread.Sleep(500);
+                recipt.Signal();
+            }
+        }
+        //Start
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //standingArea.Items.Clear();
+            //eventLog.Items.Clear();
+            //sofaList.Items.Clear();
+            //cashierList.Items.Clear();
+            //outside.Items.Clear();
+            nCustomers = Int32.Parse(textBox1.Text);
+            nCap = Int32.Parse(textBox2.Text);
+            nSofa = Int32.Parse(textBox3.Text);
+
+            max_capacity = new Semaphore(nCap);
+            sofa = new Semaphore(nSofa);
+            barberChair = new Semaphore(3);
+            coordinator = new Semaphore(3);
+            custReady = new Semaphore(0);
+            leaveChair = new Semaphore(0);
+            payment = new Semaphore(0);
+            recipt = new Semaphore(0);
+            finished = new Semaphore[nCustomers];
+            mutex1 = new Semaphore(1);
+            mutex2 = new Semaphore(1);
+            customers = new Thread[nCustomers];
+            barbers = new Thread[nBarbers];
+            cashiers = new Thread[nCashier];
+            Q1 = new Queue();
+            Q2 = new Queue();
+            count = 1;
+            for (int i = 0; i < nCustomers; i++)
+            {
+                finished[i] = new Semaphore(nCustomers);
+            }
+
+            for (int i = 0; i < nCustomers; i++)
+            {
+                TH = new ThreadStart(customer);
+                customers[i] = new Thread(TH);
+                customers[i].Start();
+            }
+            for (int i = 0; i < nBarbers; i++)
+            {
+                TH = new ThreadStart(barber);
+                barbers[i] = new Thread(TH);
+                barbers[i].Start();
+            }
+            for (int i = 0; i < nCashier; i++)
+            {
+                TH = new ThreadStart(cashier);
+                cashiers[i] = new Thread(TH);
+                cashiers[i].Start();
+            }
+            
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < nCustomers; i++)
+            {
+                customers[i].Abort();
+            }
+            for (int i = 0; i < nBarbers; i++)
+            {
+                barbers[i].Abort();
+            }
+            for (int i = 0; i < nCashier; i++)
+            {
+                cashiers[i].Abort();
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lst_OD_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+        
+        }
+
+        private void textBox2_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
